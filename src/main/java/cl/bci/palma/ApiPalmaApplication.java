693e8641dfbb45e37bb147b60c13193c34e57d87
Pablo Palma <pablo.palma.n@gmail.com>
2022-11-16 13:35:35 -0300

    first version with structure.

---
+++ src/main/java/cl/bci/palma/ApiPalmaApplication.java
@@ -0,0 +1,13 @@
+package cl.bci.palma;
+
+import lombok.Generated;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Generated
+public class ApiPalmaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPalmaApplication.class, args);
+    }
+}
